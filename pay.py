f87554ecff1328782f10cc7e33e3962bd87d9215
Jack <40618@longroad.ac.uk>
2014-11-20 22:32:36 +0000

    Pay Starter Exercise

--- pay.py
+++ pay.py
@@ -1,7 +1,10 @@
 #Pay Calculations
 #Jack Humphreys
 
-
+def inputs():
+    hours = int(input("Please enter your hours: "))
+    pay = float(input("Please your pay rate: "))
+    return hours,pay
 
 def calculate_basic_pay(hours,pay):
     total = hours*pay
@@ -14,21 +17,23 @@ def calculate_overtime_pay(hours,pay):
     return total
 
 def calculate_total_pay(hours,pay):
-    if pay <= 40:
+    if hours <= 40:
         total = calculate_basic_pay(hours,pay)
     else:
         total = calculate_overtime_pay(hours,pay)
     return total
 
-def work_details():
-    hours = int(input("Please enter your hours: "))
-    pay = float(input("Please your pay rate: "))
-    return hours,pay
 
-def calulate_pay(hours,pay):
-    hours,pay = work_details()
-    total = calculate_total_pay(hours,pay)
+def display_total(hours,pay):
+    print(calculate_total_pay(hours,pay))
 
-def display_total(total):
-    print(total)
+
+
+hours,pay = inputs()
+calculate_basic_pay(hours,pay)
+calculate_overtime_pay(hours,pay)
+calculate_total_pay(hours,pay)
+display_total(hours,pay)
+
+
 
