90d0a6159c133df134d1a29a507c6587bd29fb9d
40618 <40618@longroad.ac.uk>
2014-11-25 13:04:33 +0000

    Seconds conversion

--- seconds_conversion.py
+++ seconds_conversion.py
@@ -1,34 +1,26 @@
 def inputs():
-    hours = int(input("Please enter hours: "))
+    hours_input = int(input("Please enter hours: "))
     mins = int(input("Please enter minutes: "))
     secs = int(input("Please enter seconds: "))
-    return hours,mins,secs
+    return hours_input,mins,secs
 
-def seconds(secs):
-    secs = secs
-    return secs
-
-def minutes(mins):
-    secs = mins*60
-    return secs
-
-def hours(hours):
-    mins = hours*60
-    secs = mins*60
-    return secs
+def hours_mins_secs(hours_input,mins,secs):
+    secs_secs = secs
+    mins_secs = mins*60
+    hours_secs = (hours_input*60)*60
+    return secs_secs,mins_secs,hours_secs
 
 def total_secs(seconds,minutes,hours):
     total = seconds+minutes+hours
     return total
 
-def display_total(total_secs,hours,mins,secs):
-    print("The total numbers of seconds of {0} hours, {1} minutes and {2} seconds is {3}".format(hours,mins,secs,total_secs))
+def display_total(total,hours_input,mins,secs):
+    print("The total numbers of seconds of {0} hours, {1} minutes and {2} seconds is {3} seconds.".format(hours_input,mins,secs,total))
 
-#Main Program
-hours,mins,secs = inputs()
-seconds = seconds(secs)
-minutes = minutes(mins)
-hours = hours(hours)
-total_secs = total_secs(seconds,minutes,hours)
+def seconds_conversion():
+    hours_input,mins,secs = inputs()
+    seconds,minutes,hours = hours_mins_secs(hours_input,mins,secs) 
+    total = total_secs(seconds,minutes,hours)
+    display_total(total,hours_input,mins,secs)
 
-
+seconds_conversion()
