2dcea64b71a4e4278b4b5073002380c28c0fb586
Jack <40618@longroad.ac.uk>
2014-12-01 22:39:52 +0000

    Diamond Generator

--- diamond_generator.py
+++ diamond_generator.py
@@ -1,6 +1,6 @@
 def inputs():
     odd_num = int(input("Please enter a odd number: "))
-    if odd_num%2 == 0:
+    if odd_num//2 == 0:
         print("This is an even number. Try again: ")
     else:
         odd_num = odd_num
@@ -8,10 +8,10 @@ def inputs():
 
 def diamond_generator(odd):
     star = ("*")
-    middle = (star*odd)
     while star > 1:
-        print(middle)
-        print(middle-2)
+        print("{0}:^".format(stars))
+        
+        
 
 odd = inputs()
 diamond_generator(odd)
